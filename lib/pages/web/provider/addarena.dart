2441f2fc0e63b00c3ade2675726339372ad12967
harrislukman13 <harrislukman13@gmail.com>
2022-06-21 01:08:44 +0800

    provider navigate file

---
+++ lib/pages/web/provider/addarena.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddArena extends StatefulWidget {
+  const AddArena({ Key? key }) : super(key: key);
+
+  @override
+  State<AddArena> createState() => _AddArenaState();
+}
+
+class _AddArenaState extends State<AddArena> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/pages/web/provider/listarena.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ListArena extends StatefulWidget {
+  const ListArena({ Key? key }) : super(key: key);
+
+  @override
+  State<ListArena> createState() => _ListArenaState();
+}
+
+class _ListArenaState extends State<ListArena> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
