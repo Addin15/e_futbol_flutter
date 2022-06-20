d066619e8948c24c452c484a7ac1c959e9a99c76
akmalhazim00 <201075@student.upm.edu.my>
2022-06-21 02:35:26 +0800

    Update listarena.dart
    list arena ui

--- lib/pages/web/provider/listarena.dart
+++ lib/pages/web/provider/listarena.dart
@@ -11,8 +11,95 @@ class ListArena extends StatefulWidget {
 class _ListArenaState extends State<ListArena> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+        return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Text(
+            'Field Name',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Grass Type',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Number light',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+         DataColumn(
+          label: Text(
+            'Subs bench',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+         DataColumn(
+          label: Text(
+            'Day Price',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+         DataColumn(
+          label: Text(
+            'Night Price',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+         DataColumn(
+          label: Text(
+            'Referee',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+         DataColumn(
+          label: Text(
+            'Spectators seat',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Ahmad')),
+            DataCell(Text('Nth')),
+            DataCell(Text('2')),
+            DataCell(Text('4')),
+            DataCell(Text('50')),
+            DataCell(Text('70')),
+            DataCell(Text('2')),
+            DataCell(Text('1')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+         DataCell(Text('Ali')),
+            DataCell(Text('Bit')),
+            DataCell(Text('1')),
+            DataCell(Text('3')),
+            DataCell(Text('60')),
+            DataCell(Text('80')),
+            DataCell(Text('2')),
+            DataCell(Text('1')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+               DataCell(Text('Aaaad')),
+            DataCell(Text('Nthaaa')),
+            DataCell(Text('2')),
+            DataCell(Text('4')),
+            DataCell(Text('50')),
+            DataCell(Text('70')),
+            DataCell(Text('2')),
+            DataCell(Text('1')),
+          ],
+        ),
+      ],
     );
   }
 }
